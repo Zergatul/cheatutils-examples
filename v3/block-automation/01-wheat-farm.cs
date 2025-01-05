f8c26c0cdab3d980b255c87089de227d842e0304
Igor Budzhak <z3rgatul@gmail.com>
2025-01-05 15:00:06 +0200

    added wheat farm script

---
+++ v3/block-automation/01-wheat-farm.cs
@@ -0,0 +1,22 @@
+// this script automatically harvest wheat
+// and replants seeds
+
+// uncomment below condition if you need delay
+/*if (game.getTick() % 5 != 0) {
+    return;
+}*/
+// checking blocks not more than 1 block difference on Y level
+if (math.abs(player.getY() - y) > 1) {
+    return;
+}
+if (game.blocks.canBeReplaced(x, y, z)) {
+    if (game.blocks.getId(x, y - 1, z) == "minecraft:farmland") {
+        blockAutomation.useItem("wheat_seeds", "from-top");
+    }
+}
+
+if (true && game.blocks.getId(x, y, z) == "minecraft:wheat") {
+    if (game.blocks.getIntegerTag(x, y, z, "age") == 7) {
+        blockAutomation.breakBlock();
+    }
+}
