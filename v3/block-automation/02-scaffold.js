781f87fb16e19cb49cb9f4d2dbd9155f963e50a0
Igor Budzhak <z3rgatul@gmail.com>
2025-02-23 12:30:14 +0200

    added Scaffold example

---
+++ v3/block-automation/02-scaffold.js
@@ -0,0 +1,10 @@
+// this script shows an example how you can code custom Scaffold
+// for example Scaffold module does not allow you to use gravity blocks
+
+let pos = player.getPosition();
+// if current block is block below us
+if (math.floor(pos.x) == x && math.floor(pos.y) - 1 == y && math.floor(pos.z) == z) {
+    if (game.blocks.canBeReplaced(x, y, z)) {
+        blockAutomation.useItem("sand");
+    }
+}
