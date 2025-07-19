fd1ae5ad207c90ba64f5e66c6f2913b4c275d1c2
Igor Budzhak <z3rgatul@gmail.com>
2025-07-19 13:12:25 +0300

    added nbt example

---
+++ v3/other/nbt.cs
@@ -0,0 +1,31 @@
+// 1.21.7+ cheatutils, since 3.10.0 version
+
+let id = player.target.getEntityId();
+if (id <= 0) {
+    return;
+}
+
+// get NBT object for entity in the crosshair
+let nbt = game.entities.getNbt(id);
+debug.write(nbt.toString());
+
+// reading some values from root of NBT object
+ui.systemMessage("Age = " + nbt["Age"].getIntOr(-999));
+ui.systemMessage("Health = " + nbt["Health"].getIntOr(-999));
+
+// reading nested object
+let pos = nbt["Pos"];
+if (pos is ListTag) {
+    let list = pos as ListTag;
+    ui.systemMessage("Pos: " + 
+        "X=" + list[0].getFloatOr(-1) + " " +
+        "Y=" + list[1].getFloatOr(-1) + " " +
+        "Z=" + list[2].getFloatOr(-1) + " ");
+} else {
+    // report typename if something went wrong
+    ui.systemMessage(#typeof(pos).name);
+}
+
+// convert to UUID object -> toString
+let uuid = (nbt["UUID"] as IntArrayTag).asUUID().toString();
+ui.systemMessage(uuid);
