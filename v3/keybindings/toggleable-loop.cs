79ec2b0e327a4136c70573ebd6cd32faba034f7f
Igor Budzhak <z3rgatul@gmail.com>
2025-07-25 20:29:05 +0300

    added toggleable loop

---
+++ v3/keybindings/toggleable-loop.cs
@@ -0,0 +1,16 @@
+// this keybinding script allows you to run something in a loop
+// and use single key as toggle for this loop
+
+static boolean active = false;
+
+async void loop() {
+    while (active) {
+        ui.systemMessage(game.getTick().toString());
+        await delay.ticks(10);
+    }
+}
+
+active = !active;
+if (active) {
+    loop();
+}
