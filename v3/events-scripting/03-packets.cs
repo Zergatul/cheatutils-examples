2dbba4cd79a860e3dcf4bd43a5400cc692c2d40f
Igor Budzhak <z3rgatul@gmail.com>
2025-08-05 19:02:58 +0300

    added packets example

---
+++ v3/events-scripting/03-packets.cs
@@ -0,0 +1,10 @@
+// cheatutils 3.11.2+, Advanced Scripting must be ON
+// below code works only in Forge/NeoForge
+// fabric still uses obfuscated class/method names at runtime
+
+events.onServerToClientPacket(event => {
+    if (event.packet is Java<net.minecraft.network.protocol.game.ClientboundSoundPacket>) {
+        let s = event.packet as Java<net.minecraft.network.protocol.game.ClientboundSoundPacket>;
+        ui.systemMessage(s.getSound().getRegisteredName());
+    }
+});
