42c5717eefe571c0d145a91d8892917d8c60aaed
Igor Budzhak <z3rgatul@gmail.com>
2025-10-28 18:00:17 +0200

    added http example

---
+++ v3/other/http.cs
@@ -0,0 +1,19 @@
+void httpGetExample() {
+    let request = HttpRequest.createBuilder()
+        .get()
+        .url("http://example.com")
+        .header("X-API-Key", "abc")
+        .build();
+    let response = await http.send(request);
+    ui.systemMessage(response.body);
+}
+
+void httpPostExample() {
+    let request = HttpRequest.createBuilder()
+        .post("post-data")
+        .url("http://example.com")
+        .header("X-API-Key", "abc")
+        .build();
+    let response = await http.send(request);
+    ui.systemMessage(response.body);
+}
