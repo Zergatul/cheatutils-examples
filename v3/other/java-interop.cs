6aebd79196507f1e69ac24506731a56aa97cf56d
Igor Budzhak <z3rgatul@gmail.com>
2025-12-30 20:53:01 +0200

    added java interop example

---
+++ v3/other/java-interop.cs
@@ -0,0 +1,54 @@
+// Minecraft: 1.21.11+
+// CheatUtils: 3.14.12+
+// Doesn't work in Fabric 1.21.11 because obfuscation is still ON
+// Will work in Minecraft 26.1 for every mod loader
+
+typealias Minecraft = Java<net.minecraft.client.Minecraft>;
+typealias Suggestions = Java<com.mojang.brigadier.suggestion.Suggestions>;
+typealias Suggestion = Java<com.mojang.brigadier.suggestion.Suggestion>;
+
+let prefix = "a";
+
+ui.systemMessage("§e[Lookup] Querying server for: '§f" + prefix + "§e'...");
+
+// get vanilla objects
+let mc = Minecraft.getInstance();
+let connection = mc.getConnection();
+
+if (connection is null) {
+    ui.systemMessage("§cError: No connection.");
+    return;
+}
+
+// use vanilla classes to get command suggestions from the server
+let dispatcher = connection.getCommands();
+let provider = connection.getSuggestionsProvider();
+let parseResults = dispatcher.parse(prefix, provider);
+let suggestionsFuture = dispatcher.getCompletionSuggestions(parseResults);
+
+let ticks = 0;
+while (!suggestionsFuture.isDone()) {
+    await delay.ticks(1);
+    ticks++;
+    // wait for future completion no more than 60 ticks
+    if (ticks > 60) {
+        ui.systemMessage("§c[Lookup] Timeout.");
+        return;
+    }
+}
+
+let suggestions = await suggestionsFuture;
+let list = suggestions.getList();
+
+if (list.isEmpty()) {
+    ui.systemMessage("§7[Lookup] No matches.");
+    return;
+}
+
+// display results as system chat messages
+for (let i = 0; i < list.size(); i++) {
+    let suggestion = #cast(list.get(i), Suggestion);
+    if (suggestion.getText().length > 1) {
+        ui.systemMessage("§a[Lookup] Found: " + suggestion.getText());
+    }
+}
